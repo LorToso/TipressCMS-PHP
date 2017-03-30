1ee8eaae647b779ba89581112000367e57a13b1b
Lorenzo Toso <LorToso@gmail.com>
2017-03-30 15:16:13 +0200

    Included production paths

--- paths.php
+++ paths.php
@@ -5,9 +5,23 @@
  * Date: 30.03.2017
  * Time: 14:47
  */
-$author_image_path      = '../tipress/img/autori/';
-$author_document_path   = '../tipress/pdf/';
-$book_image_path        = '../tipress/img/libri/';
-$book_document_path     = '../tipress/pdf/';
-$client_image_path      = '../tipress/img/clienti/';
-$client_document_path   = '../tipress/pdf/';
+$dev = true;
+if($dev)
+{
+    $author_image_path      = '../tipress/img/autori/';
+    $author_document_path   = '../tipress/pdf/';
+    $book_image_path        = '../tipress/img/libri/';
+    $book_document_path     = '../tipress/pdf/';
+    $client_image_path      = '../tipress/img/clienti/';
+    $client_document_path   = '../tipress/pdf/';
+}
+else
+{
+    $author_image_path      = '../img/autori/';
+    $author_document_path   = '../pdf/';
+    $book_image_path        = '../img/libri/';
+    $book_document_path     = '../pdf/';
+    $client_image_path      = '../img/clienti/';
+    $client_document_path   = '../pdf/';
+}
+
